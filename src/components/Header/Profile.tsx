6fc1913ff35e3cb175d1caf498d76c607e4c961e
Vinicius Mendite <vinicius.miff@gmail.com>
2021-04-15 18:00:46 -0300

    header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Vinícius Mendite</Text>
-        <Text color="gray.300" fontSize="small">
-          vinicius.47289@outlook.com
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Vinícius Mendite</Text>
+          <Text color="gray.300" fontSize="small">
+            vinicius.47289@outlook.com
         </Text>
-      </Box>
+        </Box>
+      )}
 
       <Avatar size="md" name="Vinícius Mendite" src="https://github.com/viniciusmendite.png" />
     </Flex>
